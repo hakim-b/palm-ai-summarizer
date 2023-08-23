f147d642f24e88761dd5c01acab9766f57238cc8
hakim-b <hakimbouabdellah02@gmail.com>
2023-08-23 14:40:17 -0400

    feat(system theme): can now detect ystem theme (light or dark)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,46 @@
+"use client";
+
+import { Moon, Palmtree, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 function Home() {
+  const { setTheme } = useTheme();
+
   return (
     <>
-      <h1 className="text-4xl font-bold">Hello World</h1>
+      <div className="flex justify-between shadow-md p-6">
+        <h1 className="text-4xl font-bold flex">
+          <Palmtree className="h-10 w-10"/>
+          Summarizer
+        </h1>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </>
   );
 }
--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -1,4 +1,4 @@
-import { env } from "@/env.mjs";
+import { env } from "./env.mjs";
 import { initializeApp } from "firebase/app";
 
 export const firebaseConfig = {
