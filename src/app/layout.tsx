68a3a0366b1988fc440cc9534b336258dd9bb196
hakim-b <hakimbouabdellah02@gmail.com>
2024-01-05 22:00:05 -0500

    now lets see if the og img is generated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,18 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ReactNode } from "react";
+import ogImg from "./opengraph-image.png";
 
 export const metadata: Metadata = {
   title: "PaLM AI Text Summarizer",
   description: "Built using Next.js, Shadcn UI and Firebase",
+  metadataBase: new URL("https://palm-ai-summarizer-6f2b5.web.app/"),
   keywords: ["PalM", "PaLM 2", "PaLM AI", "AI Summarizer", "Text Summarizer"],
   openGraph: {
-    url: "/src/app/opengraph-image.png",
+    title: "PaLM AI Text Summarizer",
+    description:
+      "An AI summarizer app built with Next.js, Shadcn UI and Firebase",
+    images: [{ url: ogImg.src }],
   },
 };
 
